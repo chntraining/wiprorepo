3eb6a1bc088cb6028bdc5e89ed24f4422cf53070
Deeps <deepikkaa20@gmail.com>
2024-06-21 12:37:24 +0530

    jseg14.1.js

--- webfold/jseg14.js
+++ webfold/jseg14.js
@@ -1,13 +1,29 @@
 const fs = require("fs");
 const axios = require("axios");
 const url = "https://jsonplaceholder.typicode.com/users/";
-fs.promises
-  .readFile("./myfilee.txt", { encoding: "utf-8" })
-  .then((data) => {
-    axios.get(url + data).then((resp) => {
-      console.log(resp.data);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// fs.promises
+//   .readFile("./myfile.txt", { encoding: "utf-8" })
+//   .then((data) => {
+//     axios.get(url + data).then((resp) => {
+//       console.log(resp.data);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const getUser = async () => {
+  try {
+    const data = await fs.promises.readFile("./myfile.txt", { encoding: "utf-8" });
+    const data2 = await fs.promises.readFile("./myfile2.txt", { encoding: "utf-8" });
+    // console.log(data);
+    const userData = await axios.get(url + data);
+    const userData2 = await axios.get(url + data2);
+    console.log(userData.data);
+    console.log(userData2.data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getUser();
